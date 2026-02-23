27c73ba0b7fd47109d57c53d9ac0039c10db3a0c
git config --global user.email kiritodd10@gmail.com <kiritodd10@gmail.com>
2026-02-23 22:27:50 +0700

    jobsheet 2 view

--- project_PWL/routes/web.php
+++ project_PWL/routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\WelcomeController;
 
 Route::get('/', HomeController::class);
 Route::get('/about', AboutController::class);
@@ -19,3 +20,6 @@ Route::resource('photos', PhotoController::class)->only([
 Route::resource('photos', PhotoController::class)->except([ 
 'create', 'store', 'update', 'destroy' 
 ]); 
+
+Route::get('/greeting', [WelcomeController::class, 
+'greeting']); 
